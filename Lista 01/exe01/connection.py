cb089cddf17a1f9ff103677e54011f7317aa9ad9
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-09-24 15:54:32 -0300

    refact: changed identation settings

--- Lista 01/exe01/connection.py
+++ Lista 01/exe01/connection.py
@@ -1,10 +1,12 @@
 import tweepy
 import configparser
 
+config = configparser.ConfigParser()
+config.read("./credentials/config.ini")
+
+
 def connectToTwitterAPI():
-   config = configparser.ConfigParser()
-   config.read("./credentials/config.ini")
-   consumer_key = config.get("TwitterAPI", "consumer_key")
-   consumer_secret = config.get("TwitterAPI", "consumer_secret")
-   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-   return tweepy.API(auth)
+    consumer_key = config.get("TwitterAPI", "consumer_key")
+    consumer_secret = config.get("TwitterAPI", "consumer_secret")
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    return tweepy.API(auth)
